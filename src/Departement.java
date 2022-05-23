af84cb4527340940830963fd41021bf5e7a10c25
fubukyx <fubuki6shiro@live.fr>
2022-05-23 20:44:44 +0100

    Initial commit

---
+++ src/Departement.java
@@ -0,0 +1,4 @@
+public class Departement {
+    public int id_deprat;
+    public String nom;
+}
---
+++ src/Professeur.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Professeur implements IMetier{
+    public int id_prof;
+    public String nom;
+    public String prenom;
+    public String cin;
+    public String adress;
+    public String telephone;
+    public String email;
+    public Date date_recrutement;
+}
